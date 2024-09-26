38ce970824e3e5c9f4311d6303f5cf5978e49874
0meeee0 <mehdi74.id@gmail.com>
2024-09-26 17:30:23 +0100

    feat: JWT token is being generated

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,38 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/Users');
+const User = require("../model/Users");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.get('/', (req, res)=>{
-    res.send('alo')
-})
-router.post('/api/auth/register', async(req, res)=>{
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword;
-        const client = await User.create(req.body)
-        res.status(200).json(client)
-    }catch(err){
-        res.status(500).json(err);
-    }
+router.get("/", (req, res) => {
+  res.send("alo");
 });
-router.get('/api/users', async(req, res)=>{
-    const users = await User.find({})
-    res.json(users)
+router.post("/api/auth/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const client = await User.create(req.body);
+    res.status(200).json(client);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-router.post('/api/auth/login', async(req, res)=>{
-    const {email, password} = req.body
-    if(!(email && password)){
-        return res.status(400).json({message: 'Please provide email and password'});
+router.get("/api/users", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+router.post("/api/auth/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+      return res
+        .status(400)
+        .json({ message: "Please provide email and password" });
     }
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     if (!(user && (await bcrypt.compare(password, user.password)))) {
       return res.status(404).json({ message: "Invalid credentials" });
+    } else {
+      const userId = user.id;
+      const token = jwt.sign({ userId }, "my_secret_key");
+      res.status(200).json({ token });
     }
-    res.json(`you are logged in as a ${user.role}`)
-})
-router.post('/api/auth/logout');
-router.post('/api/auth/forget');
-router.post('/api/auth/reset');
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+router.post("/api/auth/logout");
+router.post("/api/auth/forget");
+router.post("/api/auth/reset");
 
 module.exports = router;
