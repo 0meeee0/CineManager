eeccff8cb2ebd161a380a0423d8a6b80f0dc0d28
0meeee0 <mehdi74.id@gmail.com>
2024-09-26 16:55:43 +0100

    feat: users are created with encypted passwords, login password decrypting on board as well, JWT not implemented yet

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,7 +10,6 @@ router.post('/api/auth/register', async(req, res)=>{
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         req.body.password = hashedPassword;
-        // res.send(req.body);
         const client = await User.create(req.body)
         res.status(200).json(client)
     }catch(err){
@@ -21,7 +20,17 @@ router.get('/api/users', async(req, res)=>{
     const users = await User.find({})
     res.json(users)
 });
-router.post('/api/auth/login');
+router.post('/api/auth/login', async(req, res)=>{
+    const {email, password} = req.body
+    if(!(email && password)){
+        return res.status(400).json({message: 'Please provide email and password'});
+    }
+    const user = await User.findOne({email});
+    if (!(user && (await bcrypt.compare(password, user.password)))) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    }
+    res.json(`you are logged in as a ${user.role}`)
+})
 router.post('/api/auth/logout');
 router.post('/api/auth/forget');
 router.post('/api/auth/reset');
